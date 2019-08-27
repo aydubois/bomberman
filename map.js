55ad76f1ec789ec42420b35304c9a90cf013c4cf
aydubois <ay.dubois@gmail.com>
2019-08-27 17:32:50 +0200

    divPlayer / sizes map, cases

--- map.js
+++ map.js
@@ -4,34 +4,34 @@ Creation of the map
 import {randomNum} from './util.js';
 import {Player} from './players.js';
 export class Map{
-    constructor(width, height){
-        this.width = width;
-        this.height = height;
+    constructor(rows, columns){
+        this.rows = rows;
+        this.columns = columns;
     }
 
     initSet(){
+        const widthCase = 50;
         let i=0;
         const game = document.getElementById('game');
-        for(let x=0; x<this.width; x++){
-            for(let y=0; y<this.height; y++){
+        game.style.width = (widthCase * this.columns) + "px";
+        game.style.height = (widthCase * this.rows) + "px";
+        
+        const divPlayer = document.getElementById('divPlayer');
+        divPlayer.style.width = (widthCase * this.columns) + "px";
+        divPlayer.style.height = (widthCase * this.rows) + "px";
+
+        for(let y=0; y<this.rows; y++){
+            for(let x=0; x<this.columns; x++){
                 const block = document.createElement('div');
                 block.className = 'case';
-                block.setAttribute('column', y);
-                block.setAttribute('row', x);
+                block.setAttribute('column', x);
+                block.setAttribute('row', y);
                 block.setAttribute('id', i);
+
                 game.appendChild(block);
                 i++;
             }
         }
-        /*  Change map size on css 
-        const size = document.getElementsByClassName('case');
-        let j=0;
-        for(j=0; j<this.height; j++){
-            size.style.height = `"${this.height}"`;
-        }
-        for(j=0; j<this.width; j++){
-            size.style.width = `"${this.width}"`;
-        } */
         
         this.randomSoftWall(20);
         this.hardWall();
@@ -39,7 +39,7 @@ export class Map{
     }
 
     randomSoftWall(numberWall){
-        let maxBlock = this.width * this.height;
+        let maxBlock = this.columns * this.rows;
         while(numberWall > 0){
             let id = randomNum(0, (maxBlock - 1));
             const wall = document.getElementById(id);
@@ -51,30 +51,29 @@ export class Map{
     }
 
     hardWall(){
-        const wallHard = document.querySelectorAll(`[row="0"],[row="${this.width - 1}"],[column="0"],[column="${this.height - 1}"]`);
+        const wallHard = document.querySelectorAll(`[row="0"],[row="${this.rows - 1}"],[column="0"],[column="${this.columns - 1}"]`);
 
         for( let block of wallHard){
             block.style.backgroundImage = 'url("wallHard.jpg")';
             block.style.backgroundSize = "cover";
             block.style.backgroundColor = "black";
         }
-
     }
 
     initPlayer(){
-        let maxBlock = this.width * this.height;
-        for(let i=0; i<(maxBlock-1); i++){
-            const block = document.getElementById(i);
-            if(!block.style.backgroundImage ){
-                block.style.backgroundImage = 'url("player1.png")';
-                block.style.backgroundSize = "cover";
-                block.setAttribute("name", "player1");
-            
+        const player = document.createElement('div');
+        player.style.position = "absolute";
+        player.className = 'player';
+        player.style.width = "50px"; // A changer si changment dans scss
+        player.style.height = "50px";
+        player.style.left = "0";
+        player.style.right = "0";
+        player.style.backgroundImage = 'url("player1.png")';
+        player.style.backgroundSize = "cover";
+        const divPlayer = document.getElementById("divPlayer");
+        divPlayer.appendChild(player);
 
-                let player1 = new Player();
-                break;
-            }
-        }
+        let player1 = new Player();
     }
 }
 
